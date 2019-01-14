12b309b4718bfb1e5dfcf2f805903646d2b61166
TiboRu <thibault.ruby@outlook.fr>
2019-01-14 13:59:19 +0100

    Script now set camera resolution, and stores the file using the env var FIYELI_IMAGES

--- camera.py
+++ camera.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env  python
 from picamera import PiCamera
 import time
+import os
 
 camera = PiCamera()
 
+# Set the camera resolution in order to have processable images
+camera.resolution = (1024, 768)
+
 camera.start_preview()
-camera.capture("img/" + str(int(time.time())) + ".jpg")
+time.sleep(2) # Warming-up camera
+# Capturing image and storing in the right place and with the right name (<timestamp>.jpg)
+camera.capture(os.getenv('FIYELI_IMAGES') + "/" + str(int(time.time())) + ".jpg")
 camera.stop_preview() 
